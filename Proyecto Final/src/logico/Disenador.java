994b75387bd6aa75bc10aacf52eff9b39b09c873
Janner <Janner@DESKTOP-EA1AT7V>
2024-03-22 23:43:04 -0400

    X2

---
+++ Proyecto Final/src/logico/Disenador.java
@@ -0,0 +1,19 @@
+package logico;
+
+public class Disenador {
+	    private int aniosExperiencia;
+	    
+	    public Disenador(String id, String nombre, String apellidos, String direccion, char sexo, int edad, double salarioHora, String evaluacion, int aniosExperiencia) {
+	        super();
+	        this.aniosExperiencia = aniosExperiencia;
+	    }
+	    public int getAñosExperiencia() {
+	        return aniosExperiencia;
+	    }
+
+	    public void setAñosExperiencia(int añosExperiencia) {
+	        this.aniosExperiencia = añosExperiencia;
+	    }
+}
+
+
