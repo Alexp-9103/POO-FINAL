79a1c24f0ebf2b1b2fd29556022ffce1855f4095
Juan Alexander <juanalexanderalejopolonia@gmail.com>
2024-03-22 21:44:11 -0400

    new classes

---
+++ Proyecto Final/src/logico/Cliente.java
@@ -0,0 +1,60 @@
+package logico;
+import java.util.ArrayList;
+
+public class Cliente {
+	
+	String id;
+	String nombre;
+	String direccion;
+	ArrayList <Proyecto> misProyectos;
+
+	
+	public Cliente(String id, String nombre, String direccion, ArrayList<Proyecto> misProyectos) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.misProyectos = misProyectos;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+	public ArrayList<Proyecto> getMisProyectos() {
+		return misProyectos;
+	}
+
+
+	public void setMisProyectos(ArrayList<Proyecto> misProyectos) {
+		this.misProyectos = misProyectos;
+	}
+	
+}
---
+++ Proyecto Final/src/logico/Proyecto.java
@@ -0,0 +1,14 @@
+package logico;
+
+import java.util.ArrayList;
+
+public class Proyecto {
+	
+	String idCliente;
+	String nombre;
+	int cantTrabajadores;
+	boolean contratoActivo;
+	ArrayList <Trabajador> losTrabajadores;
+	
+
+}
