2eefb03c20932874a7b889144c4d9870ec67ff14
Juan Alexander <juanalexanderalejopolonia@gmail.com>
2024-03-22 23:33:23 -0400

    .

--- Proyecto Final/src/logico/Disenador.java
+++ Proyecto Final/src/logico/Disenador.java
@@ -4,7 +4,7 @@ class Disenador extends Trabajador {
     private int anosExperiencia;
     
     public Disenador(String id, String nombre, String apellidos, String direccion, char sexo, int edad, double salarioHora, String evaluacion, int anosExperiencia) {
-        super(id, nombre, apellidos, direccion, sexo, edad, salarioHora, evaluacion, salarioHora);
+        super(id, nombre, apellidos, direccion, sexo, edad, salarioHora, evaluacion);
         this.anosExperiencia = anosExperiencia;
     }
     public int getAnosExperiencia() {
