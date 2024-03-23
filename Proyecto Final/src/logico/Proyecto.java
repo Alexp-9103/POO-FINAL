0cf15dd09925bccb48091d04c254b262f58f9bcc
Juan Alexander <juanalexanderalejopolonia@gmail.com>
2024-03-22 21:45:41 -0400

    new

--- Proyecto Final/src/logico/Proyecto.java
+++ Proyecto Final/src/logico/Proyecto.java
@@ -10,5 +10,57 @@ public class Proyecto {
 	boolean contratoActivo;
 	ArrayList <Trabajador> losTrabajadores;
 	
+	public Proyecto(String idCliente, String nombre, int cantTrabajadores, boolean contratoActivo,
+			ArrayList<Trabajador> losTrabajadores) {
+		super();
+		this.idCliente = idCliente;
+		this.nombre = nombre;
+		this.cantTrabajadores = cantTrabajadores;
+		this.contratoActivo = contratoActivo;
+		this.losTrabajadores = losTrabajadores;
+	}
+
+	public String getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(String idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCantTrabajadores() {
+		return cantTrabajadores;
+	}
+
+	public void setCantTrabajadores(int cantTrabajadores) {
+		this.cantTrabajadores = cantTrabajadores;
+	}
+
+	public boolean isContratoActivo() {
+		return contratoActivo;
+	}
+
+	public void setContratoActivo(boolean contratoActivo) {
+		this.contratoActivo = contratoActivo;
+	}
+
+	public ArrayList<Trabajador> getLosTrabajadores() {
+		return losTrabajadores;
+	}
+
+	public void setLosTrabajadores(ArrayList<Trabajador> losTrabajadores) {
+		this.losTrabajadores = losTrabajadores;
+	}
+	
+	
+	
 
 }
