8035cb5bb39f5b7fdc4db1df9099463cf1941291
diaurys <diaurys17@hotmail.com>
2024-03-23 09:47:27 -0400

    Organize las variables los parametros del constructor trabajador ya que habia un salarioHora que se estaba aun y habia un get o set demas

--- Proyecto Final/src/logico/Trabajador.java
+++ Proyecto Final/src/logico/Trabajador.java
@@ -10,7 +10,7 @@ public class Trabajador {
 	private double salarioHora;
 	private String Evaluacion;
 
-	public Trabajador(String id, String Nombre, String Apellidos, String Direccion, char Sexo, int Edad, double SalarioHora, String Evaluacion) {
+	public Trabajador(String id, String Nombre, String Apellidos, String Direccion, char Sexo, int Edad, double salarioHora, String Evaluacion) {
 	    this.id = id;
 	    this.Nombre = Nombre;
 	    this.Apellido = Apellidos;
@@ -24,63 +24,63 @@ public class Trabajador {
 	public String getId() {
 		return id;
 	}
-	
+
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
 	public String getNombre() {
 		return Nombre;
 	}
-	
+
 	public void setNombre(String nombre) {
 		Nombre = nombre;
 	}
-	
+
 	public String getApellido() {
 		return Apellido;
 	}
-	
+
 	public void setApellido(String apellido) {
 		Apellido = apellido;
 	}
-	
+
 	public String getDireccion() {
 		return Direccion;
 	}
-	
+
 	public void setDireccion(String direccion) {
 		Direccion = direccion;
 	}
-	
+
 	public char getSexo() {
 		return Sexo;
 	}
-	
+
 	public void setSexo(char sexo) {
 		Sexo = sexo;
 	}
-	
+
 	public int getEdad() {
 		return Edad;
 	}
-	
+
 	public void setEdad(int edad) {
 		Edad = edad;
 	}
-	
+
 	public double getSalarioHora() {
 		return salarioHora;
 	}
-	
+
 	public void setSalarioHora(double salarioHora) {
 		this.salarioHora = salarioHora;
 	}
-	
+
 	public String getEvaluacion() {
 		return Evaluacion;
 	}
-	
+
 	public void setEvaluacion(String evaluacion) {
 		Evaluacion = evaluacion;
 	}
