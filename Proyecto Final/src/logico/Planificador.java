0d0bb426c194f352933373cbd758f794b72fd794
diaurys17 <diga0001@ce.pucmm.edu.do>
2024-03-23 23:55:46 -0400

    implementacion del atributo en trabajador llamado aniosExperiencia

--- Proyecto Final/src/logico/Planificador.java
+++ Proyecto Final/src/logico/Planificador.java
@@ -3,8 +3,8 @@ package logico;
 public class Planificador extends Trabajador {
     private int frecuenciaPlanificacion;
 
-    public Planificador(String id, String nombre, String apellidos, String direccion, char sexo, int edad, double salarioHora, String evaluacion, int frecuenciaPlanificacion) {
-        super(id, nombre, apellidos, direccion, sexo, edad, salarioHora, evaluacion);
+    public Planificador(String id, String nombre, String apellidos, String direccion, char sexo, int edad, double salarioHora, String evaluacion, int frecuenciaPlanificacion, int aniosExperiencia) {
+        super(id, nombre, apellidos, direccion, sexo, edad, salarioHora, evaluacion, aniosExperiencia);
         this.frecuenciaPlanificacion = frecuenciaPlanificacion;
     }
     public int getFrecuenciaPlanificacion() {
