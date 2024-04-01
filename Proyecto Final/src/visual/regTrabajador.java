49bad24d9591f90d71706fb27251d4739dee8b2e
Juan Alexander <juanalexanderalejopolonia@gmail.com>
2024-04-01 00:17:13 -0400

    mas cambios al visual

--- Proyecto Final/src/visual/regTrabajador.java
+++ Proyecto Final/src/visual/regTrabajador.java
@@ -2,6 +2,8 @@ package visual;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -12,6 +14,9 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JRadioButton;
 import javax.swing.JComboBox;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+import javax.swing.JSpinner;
 
 public class regTrabajador extends JDialog {
 
@@ -19,6 +24,8 @@ public class regTrabajador extends JDialog {
 	private JTextField textid;
 	private JTextField textnombre;
 	private JTextField textdireccion;
+	private JTextField textsalario;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -37,7 +44,7 @@ public class regTrabajador extends JDialog {
 	 * Create the dialog.
 	 */
 	public regTrabajador() {
-		setBounds(100, 100, 519, 462);
+		setBounds(100, 100, 519, 538);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
@@ -88,23 +95,84 @@ public class regTrabajador extends JDialog {
 			}
 			
 			JRadioButton rdbtnmasculino = new JRadioButton("Masculino");
-			rdbtnmasculino.setBounds(5, 225, 91, 25);
+			rdbtnmasculino.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					rdbtnmasculino.setSelected(false);
+					//rdbtnfemenino.setSelected(false);
+					//pnlQCilind.setVisible(false);
+					//pnlQEsf.setVisible(true);
+					
+				}
+			});
+			rdbtnmasculino.setBounds(8, 218, 91, 25);
 			panel.add(rdbtnmasculino);
 			
 			JRadioButton rdbtnfemenino = new JRadioButton("Femenino");
-			rdbtnfemenino.setBounds(97, 225, 127, 25);
+			rdbtnfemenino.setBounds(101, 218, 127, 25);
 			panel.add(rdbtnfemenino);
+			{
+				JLabel lbledad = new JLabel("Edad:");
+				lbledad.setBounds(236, 200, 56, 16);
+				panel.add(lbledad);
+			}
+			
+			JSpinner spinneredad = new JSpinner();
+			spinneredad.setBounds(236, 219, 30, 22);
+			panel.add(spinneredad);
+			
+			JLabel lblsalario = new JLabel("Salario:");
+			lblsalario.setBounds(355, 200, 56, 16);
+			panel.add(lblsalario);
+			
+			textsalario = new JTextField();
+			textsalario.setText("0");
+			textsalario.setBounds(329, 219, 116, 22);
+			panel.add(textsalario);
+			textsalario.setColumns(10);
+			{
+				textField = new JTextField();
+				textField.setEditable(false);
+				textField.setText("$");
+				textField.setBounds(309, 219, 18, 22);
+				panel.add(textField);
+				textField.setColumns(10);
+			}
 		}
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder(null, "Tipo Trabajador", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel.setBounds(22, 278, 467, 89);
+		panel.setBounds(12, 278, 477, 60);
 		contentPanel.add(panel);
 		panel.setLayout(null);
 		{
 			JComboBox comboBox = new JComboBox();
+			comboBox.setEditable(true);
 			comboBox.setBounds(12, 24, 183, 22);
 			panel.add(comboBox);
+		}
+		{
+			JPanel paneljefeProyecto = new JPanel();
+			paneljefeProyecto.setBounds(22, 348, 19, 95);
+			contentPanel.add(paneljefeProyecto);
+			paneljefeProyecto.setLayout(null);
+		}
+		{
+			JPanel paneldiseniador = new JPanel();
+			paneldiseniador.setBounds(22, 348, 467, 95);
+			contentPanel.add(paneldiseniador);
+			paneldiseniador.setLayout(null);
+		}
+		{
+			JPanel panelprogramador = new JPanel();
+			panelprogramador.setBounds(22, 348, 467, 95);
+			contentPanel.add(panelprogramador);
+			panelprogramador.setLayout(null);
+		}
+		{
+			JPanel panelplanificador = new JPanel();
+			panelplanificador.setBounds(22, 348, 467, 95);
+			contentPanel.add(panelplanificador);
+			panelplanificador.setLayout(null);
 		}
 		{
 			JPanel buttonPane = new JPanel();
